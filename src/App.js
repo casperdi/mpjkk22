035f54be1c5937654e2d973b64846af6347f3e31
Mehukattii <cassu.dikert@gmail.com>
2022-03-21 10:56:15 +0200

    :)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
-import MediaTable from './components/MediaTable';
+import Nav from './components/nav';
+import Home from './views/home';
+import Profile from './views/profile';
+import Single from './views/single';
 // add to App.js after imports
 
 const App = () => {
-  return <MediaTable />;
+  return (
+    // eslint-disable-next-line no-undef
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/single" element={<Single />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
